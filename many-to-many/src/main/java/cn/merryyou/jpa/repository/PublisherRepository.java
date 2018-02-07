f7f825dafe53f7a5893ee3d6bd51bc276bf0acf0
longfeizheng <longfei_zheng@163.com>
2018-02-07 09:10:21 +0800

    添加多对多测试

---
+++ many-to-many/src/main/java/cn/merryyou/jpa/repository/PublisherRepository.java
@@ -0,0 +1,13 @@
+package cn.merryyou.jpa.repository;
+
+import cn.merryyou.jpa.domain.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created on 2018/2/5.
+ *
+ * @author zlf
+ * @since 1.0
+ */
+public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+}
--- many-to-many/src/test/java/cn/merryyou/jpa/repository/BookRepositoryTest.java
+++ many-to-many/src/test/java/cn/merryyou/jpa/repository/BookRepositoryTest.java
@@ -3,6 +3,7 @@ package cn.merryyou.jpa.repository;
 import cn.merryyou.jpa.domain.Book;
 import cn.merryyou.jpa.domain.Publisher;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created on 2018/2/5.
@@ -24,6 +26,9 @@ public class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
 
     @Test
     public void saveTest() throws Exception {
@@ -42,9 +47,36 @@ public class BookRepositoryTest {
     }
 
     @Test
+    public void saveTest1() throws Exception{
+
+    }
+
+    @Test
+    public void findPublisherTest() throws Exception{
+        Publisher publisher = publisherRepository.findOne(24);
+        Set<Book> books = publisher.getBooks();
+        for(Book book: books){
+            log.info(book.getName()+"..."+book.getId());
+        }
+        Assert.assertNotNull(publisher);
+        Assert.assertNotNull(publisher.getName());
+    }
+
+    @Test
     public void findAllTest() throws Exception {
         for (Book book : bookRepository.findAll()) {
             log.info(book.toString());
         }
+    }
+
+    @Test
+    public void findBookTest() throws Exception{
+        Book book = bookRepository.findOne(16);
+        Set<Publisher> publishers = book.getPublishers();
+        for(Publisher publisher: publishers){
+            log.info(publisher.getName()+"..."+publisher.getId());
+        }
+        Assert.assertNotNull(book);
+        Assert.assertNotNull(book.getName());
     }
 }
