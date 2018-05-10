abc15018777b19e97975f5264b9310dd408e8385
longfeizheng <longfei_zheng@163.com>
2018-05-10 21:55:33 +0800

    启动类中增加测试方法

--- many-to-many-extra-columns/src/main/java/cn/merryyou/jpa/JpaApplication.java
+++ many-to-many-extra-columns/src/main/java/cn/merryyou/jpa/JpaApplication.java
@@ -1,7 +1,17 @@
 package cn.merryyou.jpa;
 
+import cn.merryyou.jpa.domain.Book;
+import cn.merryyou.jpa.domain.BookPublisher;
+import cn.merryyou.jpa.domain.Publisher;
+import cn.merryyou.jpa.repository.BookRepository;
+import cn.merryyou.jpa.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.transaction.Transactional;
+import java.util.Date;
 
 /**
  * Created on 2018/2/5.
@@ -10,10 +20,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  */
 @SpringBootApplication
-public class JpaApplication {
+public class JpaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
+    }
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Override
+    @Transactional
+    public void run(String... strings) throws Exception {
+        Book bookA = new Book("Book A");
+
+        Publisher publisherA = new Publisher("Publisher A");
+
+        BookPublisher bookPublisher = new BookPublisher();
+        bookPublisher.setBook(bookA);
+        bookPublisher.setPublisher(publisherA);
+        bookPublisher.setPublishedDate(new Date());
+        bookA.getBookPublishers().add(bookPublisher);
+
+        publisherRepository.save(publisherA);
+        bookRepository.save(bookA);
+
+        // test
+        System.out.println(bookA.getBookPublishers().size());
+
+        // update
+        bookA.getBookPublishers().remove(bookPublisher);
+        bookRepository.save(bookA);
+
+        // test
+        System.out.println(bookA.getBookPublishers().size());
     }
 
 }
